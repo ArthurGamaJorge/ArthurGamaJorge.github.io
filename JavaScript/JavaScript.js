713babe40b0221cc32e13972c639f7489cf22754
ArthurGamaJorge <arthurgamajorgetec@gmail.com>
2023-04-25 21:55:41 -0300

    Optimizar aparição dos conteúdos da página

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -8,14 +8,17 @@ menu.onclick = () => {
 
 
 window.sr =ScrollReveal ({
-    distance: '65px',
+    distance: '80px',
     duration: 2600,
     delay: 450,
     reset: true,
 });
 
 sr.reveal('#Texto',{delay:50, origin:'left',}); 
-sr.reveal('#DivImagem',{delay:50, origin:'top'});
+sr.reveal('#Texto2',{delay:50, origin:'left',}); 
+sr.reveal('#Corvo',{delay:50, origin:'right'});
+sr.reveal('#DivImagem',{delay:50, origin:'left'});
 sr.reveal('#SetaBaixo',{delay:50, origin:'right'});
 sr.reveal('.Container',{delay:50, origin:'left',}); 
 sr.reveal('.Projeto',{delay:50, origin:'left',}); 
+sr.reveal('.ContainerPortifolio',{delay:50, origin:'bottom',}); 
